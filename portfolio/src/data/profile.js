9cfec920920e8d3c2e0de2938dd56ba782bf933f
Raquel Farías <108026970+RaquelFarias20@users.noreply.github.com>
2026-08-14 16:45:15 -0400

    Update profile.js

--- portfolio/src/data/profile.js
+++ portfolio/src/data/profile.js
@@ -1,5 +1,6 @@
 export const profile = {
-  greeting:    'Hello! I am Raquel Farías',
-  role:        'Industrial & Product Designer',
-  specialties: 'UX/UI & Human-Centered Design | Data & Technology Implementation | SQL, ML & Digital Transformation',
-}
+  greeting: "Hello! I am Raquel Farías",
+  role: "B.S. in Industrial Design | M.S. in Management and Data Analytics | Design Ops PM & Product Designer",
+  specialties:
+    "UX/UI & Human-Centered Design | Data & Technology Implementation | SQL, ML & Digital Transformation",
+};
